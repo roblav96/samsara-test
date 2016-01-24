d9f1f0de8a1eacdf7f1e733f20d98145879c110d
roblav96 <roblav96@gmx.com>
2016-01-24 08:01:11 -0500

    sbar time

--- src/app/sbar.js
+++ src/app/sbar.js
@@ -20,7 +20,7 @@ function sBar( context ) {
 
 	this.index = 451
 	this.xDelta = this.wWidth * 0.25
-	this.yDelta = 49
+	this.height = 49
 	this.swipeYThreshold = 40
 	this.clickThreshold = 10
 
@@ -28,34 +28,7 @@ function sBar( context ) {
 		return Math.min( Math.max( x, a ), b )
 	}
 
-	this.temps = [ {}, {}, {}, {}, {}, {}, {}, {} ]
-	
-	this.sBarTemp = [ {
-		_text: "Back",
-		_icon: "ion-arrow-left-c",
-		_color: 'assertive',
-		_href: true
-	}, {
-		_text: "Boundaries",
-		_icon: "ion-log-out",
-		_href: true
-	}, {
-		_text: "Goto Map",
-		_icon: "ion-android-expand",
-		_href: true
-	}, {
-		_text: "Activities",
-		_icon: "ion-ios-pulse-strong",
-		_href: true
-	}, {
-		_text: "Submit",
-		_icon: "ion-checkmark done"
-	}, {
-		_text: "More Actions",
-		_icon: "ion-android-more-vertical",
-		_color: 'balanced'
-	} ]
-
+	this.temp = [ {}, {}, {}, {}, {}, {}, {}, {} ]
 	this.surfs = []
 	this.nodes = []
 
@@ -63,43 +36,28 @@ function sBar( context ) {
 		direction: 0
 	} )
 
-	this.touch = new TouchInput()
+	this.input = new MouseInput()
 
-	var i, len = this.sBarTemp.length
+	this.x = new Transitionable( 0 )
+	this.transform = this.x.map( function ( v ) {
+		// console.log( 'v >', v )
+		return Transform.translateX( v )
+	} )
+
+	var i, len = this.temp.length
 	for ( i = 0; i < len; i++ ) {
 
-		var icon = ''
-		if ( this.sBarTemp[ i ]._icon ) {
-			icon = ' ' + this.sBarTemp[ i ]._icon
-		}
-
-		var text = ''
-		if ( this.sBarTemp[ i ]._text ) {
-			text = this.sBarTemp[ i ]._text
-		}
-
 		this.surfs[ i ] = new Surface( {
-			content: '<div class="tabs tabs-icon-top"><a class="tab-item"><i class="icon' + icon + '"></i>' + text + '</a></div>',
-			size: [ this.xDelta, this.yDelta ]
+			content: '',
+			size: [ this.xDelta, this.height ]
 		} )
 
-		this.surfs[ i ].click = null
-		this.surfs[ i ].clickHold = null
-		this.surfs[ i ].href = null
+		this.temp[ i ].click = null
+		this.temp[ i ].clickHold = null
+		this.temp[ i ].href = null
+		this.temp[ i ].hidden = false
 
-		if ( this.sBarTemp[ i ]._href ) {
-			this.surfs[ i ].href = true
-		}
-
-		if ( this.sBarTemp[ i ]._click ) {
-			this.surfs[ i ].click = this.sBarTemp[ i ]._click
-		}
-
-		if ( this.sBarTemp[ i ]._clickHold ) {
-			this.surfs[ i ].clickHold = this.sBarTemp[ i ]._clickHold
-		}
-
-		this.touch.subscribe( this.surfs[ i ] )
+		this.input.subscribe( this.surfs[ i ] )
 
 	}
 
@@ -107,37 +65,118 @@ function sBar( context ) {
 
 
 
-	this.x = new Transitionable( 0 )
-	this.transform = this.x.map( function ( value ) {
-		// console.log( 'value >', value )
-		return Transform.translateX( value )
-	} )
-
-	this.touch.on( 'start', function ( pay ) {
-		this.x.reset( 0 )
+	this.input.on( 'start', function ( pay ) {
+		this.x.reset()
 	}.bind( this ) )
 
-	this.touch.on( 'update', function ( pay ) {
+	this.input.on( 'update', function ( pay ) {
 		// console.log( 'pay.value[ 0 ] >', pay.value[ 0 ] )
 		this.x.set( pay.value[ 0 ] )
 	}.bind( this ) )
 
-	this.touch.on( 'end', function ( pay ) {
-		this.x.reset( pay.value[ 0 ] )
-		this.x.set( 0, {
-			duration: 250,
-			curve: Curves.outBack
-		}, function () {
-			this.x.reset( 0 )
-		}.bind( this ) )
+	this.input.on( 'end', function ( pay ) {
+		this.centerIt()
 	}.bind( this ) )
 
 
+	this.update = function ( temp ) {
+
+		// if ( _$router.histArr[ 1 ] ) {
+		// 	var backable = _.parseBool( _$router.histArr[ 0 ].query.backable )
+		// 	if ( backable == true ) {
+		// 		temp[ 0 ] = {
+		// 			_text: "Go Back",
+		// 			_icon: "ion-arrow-left-c",
+		// 			_color: 'assertive',
+		// 			_href: true,
+		// 			_click: function () {
+		// 				_$router.goBack()
+		// 			}.bind( this )
+		// 		}
+		// 		_$router.histArr[ 0 ].query = _.omit( _$router.histArr[ 0 ].query, 'backable' )
+		// 	}
+		// }
+
+		var i, len = temp.length
+		for ( i = 0; i < len; i++ ) {
+
+			this.temp[ i ].href = null
+			if ( temp[ i ]._href ) {
+				this.temp[ i ].href = true
+			}
+
+			var classes = ''
+			if ( i == 1 ) {
+				classes = ' sBar-left-' + temp[ 0 ]._color
+			}
+			if ( i == 4 && temp[ 5 ]._color ) {
+				classes = ' sBar-right-' + temp[ 5 ]._color
+			}
+
+			var icon = ''
+			if ( temp[ i ]._icon ) {
+				icon = ' ' + temp[ i ]._icon
+			}
+
+			var text = ''
+			if ( temp[ i ]._text ) {
+				text = temp[ i ]._text
+			}
+
+			this.surfs[ i ].setContent( '<div class="tabs tabs-icon-top' + classes + '"><a class="tab-item"><i class="icon' + icon + '"></i>' + text + '</a></div>' )
+
+			this.temp[ i ].click = null
+			if ( temp[ i ]._click ) {
+				this.temp[ i ].click = temp[ i ]._click
+			}
+
+			this.temp[ i ].clickHold = null
+			if ( temp[ i ]._clickHold ) {
+				this.temp[ i ].clickHold = temp[ i ]._clickHold
+			}
+
+			if ( i >= 1 && i <= 4 && this.temp[ i ]._hidden == true ) {
+				this.show( i )
+			}
+
+		}
+
+		this.surfs[ 0 ].addClass( [ 'sBar-' + temp[ 0 ]._color ] )
+		if ( temp[ 5 ]._color ) {
+			this.surfs[ 5 ].addClass( [ 'sBar-' + temp[ 5 ]._color ] )
+		}
+
+	}
+
+
+
+	this.centerIt = function ( now ) {
+		if ( now == true ) {
+			this.x.reset()
+			return
+		}
+
+		this.x.set( 0, {
+			duration: 250,
+			curve: Curves.outBack
+		} )
+	}
+
+	this.show = function ( i ) {
+		this.surfs[ i ].removeClass( 'sbar-hide' )
+		this.temp[ i ]._hidden = false
+	}
+
+	this.hide = function ( i ) {
+		this.surfs[ i ].addClass( 'sbar-hide' )
+		this.temp[ i ]._hidden = true
+	}
 
 
 
 	context.add( {
-		transform: Transform.translate( [ -this.xDelta, this.wHeight - this.yDelta ] )
+		align: [ 0, 1 ],
+		transform: Transform.translate( [ -this.xDelta, -this.height ] )
 	} ).add( {
 		transform: this.transform
 	} ).add( this.layout )
