9c3233b61627c3098f050ecad1e2eb96bb612c5b
roblav96_macpro <roblav96@gmx.com>
2016-01-24 02:09:22 -0500

    work it

--- src/app/prog.js
+++ src/app/prog.js
@@ -25,52 +25,65 @@ function Prog( context ) {
 	this.width = this.wWidth * 0.25
 	this.height = 5
 
+
+
+	this.x = new Transitionable( this.posX )
+	this.opa = new Transitionable( 1 )
+
+	this.xTrans = this.x.map( function ( v ) {
+		return Transform.translateX( v )
+	} )
+
 	this.surf = new Surface( {
 		size: [ this.width, this.height ],
 		origin: [ 0.5, 0 ],
+		opacity: this.opa,
+		attributes: {
+			id: 'prog_surf'
+		},
 		properties: {
 			background: '#11C1F3'
 		}
 	} )
 
-	this.x = new Transitionable( this.posX )
-	this.xTrans = this.x.map( function ( v ) {
-		return Transform.translateX( v )
-	} )
-	
 	this.swap = function () {
 		if ( this.active == false || this.err == true ) {
 			return
 		}
 
-		this.x.halt()
-		// this.x.set( this.posX )
-		// this.x.reset( this.posX )
 		this.posX = ( this.posX ) ? 0 : this.wWidth
 		this.x.set( this.posX, {
 			duration: 1000,
 			curve: 'easeOutBounce'
 		}, function () {
-			console.warn( 'end swap' )
+			if ( this.active == false ) {
+				this.x.reset( 0 )
+				this.x.set( 0 )
+				return
+			}
+
 			this.swap()
 		}.bind( this ) )
-
 	}
 
 	this.done = function () {
-		this.posX = 0
 		this.active = false
-		console.warn( 'done' )
-		this.x.reset( 0 )
+		this.posX = 0
+		this.opa.set( 1 )
+		
+		
 	}
 
 	this.start = function () {
 		if ( this.active == true || this.err == true ) {
 			return
 		}
-		console.warn( 'start' )
 
+		this.opa.set( 1 )
+		this.posX = 0
 		this.active = true
+
+		this.x.reset( 0 )
 		this.swap()
 	}
 
@@ -78,15 +91,12 @@ function Prog( context ) {
 		if ( this.active == false || this.err == true ) {
 			return
 		}
-		console.warn( 'stop' )
 
 		this.done()
 	}
 
 	this.error = function () {
-		this.err = true
-		// make cool red animation
-		this.err = false
+
 	}
 
 	context.add( {
