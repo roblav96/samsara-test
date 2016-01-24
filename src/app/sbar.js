84dd546d0efe007c6d49e8ce37d271c04b953a3f
roblav96_macpro <roblav96@gmx.com>
2016-01-24 09:03:31 -0500

    needs 2 scalee

--- src/app/sbar.js
+++ src/app/sbar.js
@@ -4,6 +4,7 @@ var Curves = require( './Curves.js' )
 var Samsara = require( 'samsarajs' )
 var Surface = Samsara.DOM.Surface
 var Transform = Samsara.Core.Transform
+var Timer = Samsara.Core.Timer
 var MouseInput = Samsara.Inputs.MouseInput
 var TouchInput = Samsara.Inputs.TouchInput
 var SequentialLayout = Samsara.Layouts.SequentialLayout
@@ -36,7 +37,7 @@ function sBar( context ) {
 		direction: 0
 	} )
 
-	this.input = new MouseInput()
+	this.input = new TouchInput()
 
 	this.x = new Transitionable( 0 )
 	this.transform = this.x.map( function ( v ) {
@@ -63,21 +64,216 @@ function sBar( context ) {
 
 	this.layout.addItems( this.surfs )
 
+	this.clickHoldFn = function ( i ) {
+		if ( this.absAccu > this.clickThreshold || this.didClick == true ) {
+			return
+		}
 
+		console.warn( 'this.clickHoldFn' )
+		return
+
+		if ( _.isFunction( this.temp[ i ].clickHold ) ) {
+
+			this.didClick = true
+			this.stopDragging = true
+
+			navigator.vibrate( 50 )
+
+			this.temp[ i ].clickHold()
+			this.didClick = true
+
+			// this.temp[ i ].scaleComp.set( 1.5, 1.5, 1 )
+			// this.temp[ i ].scaleComp.set( 1, 1, 1, {
+			// 	duration: 1000,
+			// 	curve: 'outBounce'
+			// } )
+
+			// this.temp[ i ].posComp.setY( -73.5 )
+			// this.temp[ i ].posComp.setY( 0, {
+			// 	duration: 1000,
+			// 	curve: 'outBounce'
+			// }, function () {
+			// 	this.centerIt()
+			// }.bind( this ) )
+
+		}
+
+	}
 
 	this.input.on( 'start', function ( pay ) {
+		console.warn( 'START > pay >', JSON.stringify( pay, true, 4 ) )
+
 		this.x.reset()
+
+		this.down = 0
+		this.absAccu = 0
+		this.accu = 0
+		this.didClick = false
+		this.stopDragging = false
+
+		this.timeout = Timer.after( this.clickHoldFn, 25 )
+
 	}.bind( this ) )
 
 	this.input.on( 'update', function ( pay ) {
-		
-		
-		
-		this.x.set( pay.value[ 0 ] )
+		// console.log( 'UPDATE > pay >', JSON.stringify( pay, true, 4 ) )
+
+		if ( this.stopDragging == true ) {
+			return
+		}
+
+		var delta = pay.delta
+		var velocity = pay.velocity
+		var value = pay.value
+
+		if ( this.down < this.swipeYThreshold ) {
+			this.down = this.down + ( ( delta[ 1 ] * velocity[ 1 ] ) - ( delta[ 0 ] * velocity[ 0 ] ) )
+				// console.log( 'this.down >', this.down )
+		} else {
+			if ( value[ 1 ] > 0 ) {
+				// _$famous.mMenu.show()
+				console.warn( '_$famous.mMenu.show' )
+			} else {
+				// self.tuts.emit()
+				console.warn( 'self.tuts.emit' )
+			}
+			this.stopDragging = true
+			this.didClick = true
+			this.centerIt()
+			return
+		}
+
+		if ( this.absAccu < this.clickThreshold ) { // for getting a threshold on click events
+			this.absAccu = this.absAccu + Math.abs( delta[ 0 ] ) + Math.abs( delta[ 1 ] )
+		}
+
+		var boost = this.clamp( Math.abs( velocity[ 0 ] * 1.25 ), 1, 3 )
+		var accu = this.accu + ( delta[ 0 ] * boost )
+		this.accu = this.clamp( accu, -this.xDelta, this.xDelta )
+		this.x.set( this.accu )
+
+
+
+		if ( Math.abs( this.accu ) >= this.xDelta ) {
+
+			if ( this.accu > 0 ) { // test if its left or right
+
+				if ( _.isFunction( this.temp[ 0 ].click ) ) {
+					var classes = this.surfs[ 0 ].getClassList()
+					if ( classes.indexOf( 'sBar-active' ) == -1 ) {
+						this.surfs[ 0 ].addClass( 'sBar-active' )
+					}
+
+					this.didClick = true
+					this.stopDragging = true
+
+					if ( this.temp[ 0 ].href != true ) {
+						this.temp[ 0 ].click()
+						Timer.after( function () {
+							this.centerIt()
+						}.bind( this ), 5 )
+						return
+					}
+
+					Timer.after( function () {
+						this.temp[ 0 ].click()
+					}.bind( this ), 5 )
+				}
+
+			} else {
+
+				if ( _.isFunction( this.temp[ 5 ].click ) ) {
+					var classes = this.surfs[ 5 ].getClassList()
+					if ( classes.indexOf( 'sBar-active' ) == -1 ) {
+						this.surfs[ 5 ].addClass( 'sBar-active' )
+					}
+
+					this.didClick = true
+					this.stopDragging = true
+
+					if ( this.temp[ 5 ].href != true ) {
+						this.temp[ 5 ].click()
+						Timer.after( function () {
+							this.centerIt()
+						}.bind( this ), 5 )
+						return
+					}
+
+					Timer.after( function () {
+						this.temp[ 5 ].click()
+					}.bind( this ), 5 )
+
+				}
+			}
+
+			return
+		}
+
+		if ( this.accu > 0 ) { // test if its left or right
+			var classes = this.surfs[ 0 ].getClassList()
+			if ( classes.indexOf( 'sBar-active' ) != -1 ) {
+				this.surfs[ 0 ].removeClass( 'sBar-active' )
+			}
+		} else {
+			var classes = this.surfs[ 5 ].getClassList()
+			if ( classes.indexOf( 'sBar-active' ) != -1 ) {
+				this.surfs[ 5 ].removeClass( 'sBar-active' )
+			}
+		}
+
 	}.bind( this ) )
 
 	this.input.on( 'end', function ( pay ) {
+		console.warn( 'END > pay >', JSON.stringify( pay, true, 4 ) )
+
+		if ( this.timeout ) {
+			Timer.clear( this.timeout )
+			this.timeout = null
+		}
+
+		if ( this.didClick == true ) {
+			return
+		}
+
+		if ( this.absAccu < this.clickThreshold ) {
+
+			var i = this.getSurfByPos( pay.clientX )
+
+			if ( _.isFunction( this.temp[ i ].click ) ) {
+
+				this.didClick = true
+
+				if ( this.temp[ i ].href == true ) {
+					this.temp[ i ].scaleComp.set( 1.5, 1.5, 1 )
+					_.delay( function () {
+						this.temp[ i ].click()
+						_.delay( function () {
+							this.temp[ i ].scaleComp.set( 1, 1, 1 )
+							this.centerIt()
+						}.bind( this ), 50 )
+					}.bind( this ), 50 )
+					return
+				}
+
+				this.temp[ i ].click()
+				this.temp[ i ].scaleComp.set( 1.5, 1.5, 1, {
+					duration: 100,
+					curve: 'easeOutBounce'
+				}, function () {
+					this.temp[ i ].scaleComp.set( 1, 1, 1, {
+						duration: 100,
+						curve: 'easeIn'
+					}, function () {
+						this.centerIt()
+					}.bind( this ) )
+				}.bind( this ) )
+
+				return
+			}
+		}
+
 		this.centerIt()
+
 	}.bind( this ) )
 
 
@@ -172,6 +368,21 @@ function sBar( context ) {
 	this.hide = function ( i ) {
 		this.surfs[ i ].addClass( 'sbar-hide' )
 		this.temp[ i ]._hidden = true
+	}
+
+	this.getSurfByPos = function ( posX ) {
+		if ( _.inRange( posX, 0, this.xDelta ) ) {
+			return 1
+		} else if ( _.inRange( posX, this.xDelta, this.xDelta * 2 ) ) {
+			return 2
+		} else if ( _.inRange( posX, this.xDelta * 2, this.xDelta * 3 ) ) {
+			return 3
+		} else if ( _.inRange( posX, this.xDelta * 3, this.xDelta * 4 ) ) {
+			return 4
+		} else {
+			console.error( 'this.getSurfByPos >' )
+			console.error( 'WTF NOT IN RANGE???' )
+		}
 	}
 
 
