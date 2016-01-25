c220f4e34bfcd210ebe9f4c257db798fa9f51609
roblav96 <roblav96@gmx.com>
2016-01-25 08:17:49 -0500

    pueigru

--- src/app/db-object.js
+++ src/app/db-object.js
@@ -30,23 +30,21 @@ function DB( opts ) {
 		unique: this.opts.indices[ 0 ]
 	} )
 
-	// var xid = Lockr.get( 'user.xid' )
-	// if ( _.isUndefined( xid ) ) {
-	// 	return
-	// }
+	var xid = Lockr.get( 'user.xid' )
+	if ( _.isUndefined( xid ) ) {
+		// return
+	}
 
 	var that = {
 		opts: this.opts,
 		loki: this.loki
 	}
 
-	if ( this.opts.name == 'geo' ) {
-		console.log( 'this.loki.data >', this.loki.data )
-		
-		return
-	}
+	this.ready = Promise.resolve().bind( that ).then( function () {
 
-	Promise.resolve().bind( that ).then( function () {
+		if ( this.opts.name == 'geo' ) {
+			throw new RangeError()
+		}
 
 		if ( this.opts.name == 'activities' ) {
 			return dexie[ this.opts.name ].orderBy( 'stamp' ).reverse().limit( 50 ).toArray()
@@ -57,7 +55,7 @@ function DB( opts ) {
 	} ).then( function ( docs ) {
 
 		if ( _.isEmpty( docs ) ) { // dont load any geo positions into memory
-			return null
+			throw new RangeError()
 		}
 
 		var i, len = docs.length
@@ -65,15 +63,37 @@ function DB( opts ) {
 			this.loki.insert( docs[ i ] )
 		}
 
+		throw new RangeError()
+
+	} ).catch( RangeError, function () {
+
 		console.log( 'this.loki.data >', this.loki.data )
+
+		return Promise.resolve( this.loki.data )
 
 	} ).catch( function ( err ) {
 		console.error( err )
 		console.error( err.stack )
 	} )
+	
+	this.ready.then( function ( idk ) {
+		console.log( 'idk >', idk )
+	} )
 
 
 }
+
+
+
+DB.prototype.init = function () {
+	console.warn( 'INITGEO >' )
+
+
+
+
+
+}
+
 
 DB.prototype.initGeo = function () {
 	console.warn( 'INITGEO >' )
